3ce0e1262fe14853b9eec1c4e7a6983e8eaea2ee
Rebecca Manzi <54328096+rebeccamanzi@users.noreply.github.com>
2022-03-05 17:36:07 -0300

    update token

--- src/services/config.js
+++ src/services/config.js
@@ -4,8 +4,7 @@ import axios from 'axios'
 
 export const instance = axios.create({
     baseURL: 'https://gorest.co.in/public/v2/',
-    headers: {
-        'Authorization': 'Bearer c5b962bb78f0345de73bd4ab12f29883c1367c017ccfee2d3b3f25c04aad9419' 
-        //'Authorization': 'Bearer ' +(token)
+    headers: {        
+        'Authorization': 'Bearer ' +(token)
     }
 })
